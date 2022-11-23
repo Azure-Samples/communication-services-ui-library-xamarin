38f0d5b3d6461a5c019beb82e392b2f5b0d94b0a
Josh Lai <joshlai@microsoft.com>
2022-11-23 11:23:11 -0800

    [MAUI] POC check in

--- CommunicationCallingXamarinSampleApp/CommunicationCallingXamarinSampleApp/IComposite.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace CommunicationCallingXamarinSampleApp
-{
-    public interface IComposite
-    {
-        void joinCall(string name, string acsToken, string callID, bool isTeamsCall, LocalizationProps? localization, DataModelInjectionProps? dataModelInjection);
-        List<string> languages();
-    }
-}
---
+++ MyMauiApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MyMauiApp.Platforms.iOS;
+using System;
+namespace MyMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Composite composite = new Composite();
+		string name = "";
+		string acsToken = "";
+		string callId = "";
+		bool isTeamsCall = true;
+		composite.JoinCall(name, acsToken, callId, isTeamsCall);
+	}
+}
+
+
